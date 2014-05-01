05601ba1222ce4b2f8148667ac57533310bb19be
aodin <aaronoellis@gmail.com>
2014-05-01 09:53:45 -0600

    Moved template parsing into the server construction function and made the parsed template a field of the server struct

--- server.go
+++ server.go
@@ -6,20 +6,37 @@ import (
 )
 
 type Server struct {
-	Output string
+	tmpl *template.Template
 }
 
 type Attrs struct {
 	StaticURL string
 }
 
-var tmpl = template.Must(template.ParseFiles("./shittystartup/templates/landing.html"))
-
 func (s *Server) WriteResponse(w http.ResponseWriter, r *http.Request) {
 	attrs := Attrs{StaticURL: "/static/"}
-	tmpl.Execute(w, attrs)
+	s.tmpl.Execute(w, attrs) // Execute returns an error that we're ignoring
 }
 
-func NewServer(output string) *Server {
-	return &Server{output}
+func NewServer() (*Server, error) {
+	// Create a pointer to a new, zero-initialized Server struct
+	server := &Server{}
+
+	// Since "tmpl" is lowercase it can only be accessed within the struct
+	tmpl, err := template.ParseFiles("./shittystartup/templates/landing.html")
+
+	// The standard error checking practice
+	if err != nil {
+		// Either return the "incomplete" server or nil, it's up to you
+		// The user of the package should be checking for returned errors
+		// before making use of the returned server struct.
+		return server, err
+	}
+
+	// We don't use assignment (:=) because the variable "server.tmpl"
+	// already exists. It was zero initialized in the server struct.
+	server.tmpl = tmpl
+
+	// If there is no error to return, return nil
+	return server, nil
 }
